3bc4f2fbf8bd69e253b774d732c60fa1868f44a2
lexineivory <31803657-lexineivory@users.noreply.replit.com>
2024-02-03 02:52:10 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstName = "Ralph";
+    string lastName = "Lee";
+    int age = 18
+    Console.WriteLine ($"Hello World {firstName} {lastName} Age: {age}");
+    Console.WriteLine ("Hello World " + firstName + " " + lastName + " " + age);
+  }
+}
