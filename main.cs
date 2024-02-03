c570f96afefd4ae7ed3e11e8b27c1ab32e08ad99
lexineivory <31803657-lexineivory@users.noreply.replit.com>
2024-02-03 02:55:45 +0000

    Added age

--- main.cs
+++ main.cs
@@ -4,7 +4,7 @@ class Program {
   public static void Main (string[] args) {
     string firstName = "Ralph";
     string lastName = "Lee";
-    int age = 18
+    int age = 18;
     Console.WriteLine ($"Hello World {firstName} {lastName} Age: {age}");
     Console.WriteLine ("Hello World " + firstName + " " + lastName + " " + age);
   }
